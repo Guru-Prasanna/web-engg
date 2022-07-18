06021dfecc3576aebd8872c28b3cdfa428f7e598
guruprasanna_v <86765245+Guru-Prasanna@users.noreply.github.com>
2022-07-18 15:06:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.cookie = "username=John Doe; expires=Thu, 18 Dec 2022 12:00:00 UTC";
 console.log("Hello");
 console.log(document.cookie); 
 }
+
+function clipboard(){
+    navigator.clipboard.readText()
+    .then(text => {
+      console.log('Pasted content: ', text);
+  })
+    .catch(err => {
+      console.error('Failed to read clipboard contents: ', err);
+    });
+  
+}
